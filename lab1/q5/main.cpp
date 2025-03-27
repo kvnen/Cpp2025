9e4c7aa2f2ee0fdb3efc221d035618977783276f
kvnen <koivunen.anttisakari@gmail.com>
2025-03-27 15:02:51 +0200

    did q5

--- lab1/q5/main.cpp
+++ lab1/q5/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream> 
 
-int main(int argc, char **argv){
+int main(void){
+	std::string input;
 	int output = 0;
-	for (int i; i < sizeof(*argv[1]); i++){
-
-	output += argv[1][i];
-
+	//asking the numbers without any whitespace since that was the example input
+	std::cout << "give numbers (without any whitespace): ";
+	// getting the input 
+	std::cin >> input; 
+	//iterating through the characters and adding them to output 
+	for(int i = 0; i < input.size(); i++){
+		// here the input characters are added to output but have to remember to -0 since the characters are ascii
+		output += input[i]-'0';	
 	}
-	std::cout << output << std::endl;
+	//printing output
+	std::cout << "the sum is: " << output << std::endl;
 	return 0;
 }
