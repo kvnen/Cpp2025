fecb746366fde34e339b099804b1503517700b31
kvnen <koivunen.anttisakari@gmail.com>
2025-03-30 09:09:14 +0300

    added comments to q1

--- lab1/q1/main.cpp
+++ lab1/q1/main.cpp
@@ -1,19 +1,24 @@
 #include <string>
 #include <iostream>
 
+// namespace for math operations
 namespace MathOperations{
+	//function that returns the sum of 2 numbers
 int add(int x, int y){
 	return x + y;
 }
 }
 
+//namespace for text operations
 namespace TextOperations {
+	//functions that concatenates 2 strings
 std::string concat(std::string a, std::string b){
 	return a + b;
 }
 }
 
 int main(void){
+	//testing the functions
 	std::cout << MathOperations::add(11,12) << std::endl;
 	std::cout << TextOperations::concat("hello", "world") << std::endl;
 }
