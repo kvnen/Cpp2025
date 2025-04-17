10165aabf3117f614278b512023b826f634169b9
kvnen <koivunen.anttisakari@gmail.com>
2025-04-17 14:17:55 +0300

    partly did lab3 q4

--- lab3/q4/main.cpp
+++ lab3/q4/main.cpp
@@ -32,10 +32,48 @@ class Car : public Vehicle {
 		void startEngine() override {
 			std::cout << "Car engine is starting!" << std::endl;
 		}
+
+		int getnumDoors(){
+			return numDoors;
+		}
 };
 
 class ElectricCar : public Car {
 	private: 
 		int batteryCapacity;
-		
+	public: 
+		ElectricCar(std::string b, int y, int n) : Car(b,y,n), batteryCapacity(0){}
+		ElectricCar(std::string b, int y, int n, int c) : Car(b,y,n), batteryCapacity(c){}
+
+		void showInfo() override {
+			std::cout << "brand: " << brand << ", year: " << year << ", number of doors: " << getnumDoors() << ", battery capacity: " << batteryCapacity << std::endl;
+		}
+
+		void startEngine() override {
+			std::cout << "Electric engine is starting... silent but powerful" << std::endl;
+		}
 };
+
+int main(){
+	Vehicle* v1 = new Vehicle("wroom", 2000);
+	v1->startEngine();
+	v1->showInfo();
+
+	Car* c1 = new Car("Toyota", 1987, 2);
+	c1->showInfo();
+	c1->startEngine();
+
+	ElectricCar* e1 = new ElectricCar("Skoda", 2024, 4);
+	ElectricCar* e2 = new ElectricCar("Skoda", 2024, 4, 1000);
+	
+	e1->showInfo();
+	e1->startEngine();
+
+	e2->showInfo();
+	e2->startEngine();
+
+	delete v1;
+	delete c1;
+	delete e1;
+	delete e2;
+}
